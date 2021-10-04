c5be9b4233fff353574ba805f89eb088d0f194da
hcb1999 <79966891+hcb1999@users.noreply.github.com>
2021-10-04 18:18:04 +0900

    First Commit

---
+++ app/src/main/java/com/cookandroid/startface/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.startface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
